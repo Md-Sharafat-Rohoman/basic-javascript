c512101973e29f5adfdb8ec8bb6c15d107432fe1
Md Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2025-04-09 23:10:46 +0600

    second commit

--- name.js
+++ name.js
@@ -1,1 +1,8 @@
 console.log('sharafat rohoman is a good student'); 
+
+
+var arrays = ['sharafat', 'rohoman', 'tamim', 'yeasin'];
+console.log(array);
+for(var array of arrays){
+    console.log(array.length);
+}
