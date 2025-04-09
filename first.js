2d6ab1a60e1a99bc0499bca3f04e19b84efb6156
Md Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2025-04-09 22:50:59 +0600

    name commit

--- first.js
+++ first.js
@@ -2,3 +2,24 @@ console.log('sharafat rohoman');
 
 var name = 'muntaha siddika fariha';
 console.log(name);
+
+
+/**
+ * **********************
+ * number 
+ * integer
+ * float
+ * operator
+ * addition
+ * substruction
+ * multiplication
+ * division
+ * percentage
+ * sum
+ * avg
+ * min
+ * max
+ * area:triangle
+ * graph
+ * 
+*/
